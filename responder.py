8982dd4db4926f3383d5b94576fdf1ec10376948
Yuhao <rogelio1026zyh@gmail.com>
2016-04-09 23:37:49 -0400

    if

--- responder.py
+++ responder.py
@@ -1,6 +1,9 @@
 print(
 """I'm Yuhao Zheng
 This is my first program
-"""
-)
-x=input("Enter some data: ")
+""")
+x=input('Please Enter:')
+if [int(x)==x]:
+ print ('This is an index')
+else:
+ print ('This is a string')
