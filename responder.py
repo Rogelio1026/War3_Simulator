f2270ecc80b2f37821f4c275e651de712e9a7e15
Yuhao <rogelio1026zyh@gmail.com>
2016-04-10 00:05:45 -0400

    wrong one

--- responder.py
+++ responder.py
@@ -3,7 +3,9 @@ print(
 This is my first program
 """)
 x=input('Please Enter:')
-if [int(x)==x]:
- print ('This is an index')
+if [x-x%1<1] and [(x%1)!=0]:
+ print ('This is a float')
+elif [int(x)==x]:
+ print ('This is an integer')
 else:
  print ('This is a string')
