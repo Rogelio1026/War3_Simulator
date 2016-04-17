936d89e426f337ec164216499978a66684fa5d33
Yuhao <rogelio1026zyh@gmail.com>
2016-04-17 13:08:31 -0400

    final

--- responder.py
+++ responder.py
@@ -1,15 +1,15 @@
 print(
 """I'm Yuhao Zheng
-This is my first program
+This is my first program -- responder
 """)
-x=raw_input('Please Enter:')
-while true:
+while True:
+ x=raw_input('Please Enter:')
+ if x=='end':break
  try:
   y=float(x)
-  if x=='end':break
   if y%1>0:
    print ('This is a float')
   else:
    print ('This is an integer')
  except ValueError, e:
-  print ('This is a string')
+  print ("This is a string")
