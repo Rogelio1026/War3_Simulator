6df937ecaeb318e7848490b95209d8b4da00bc5a
Yuhao <rogelio1026zyh@gmail.com>
2016-04-17 12:59:47 -0400

    while

--- responder.py
+++ responder.py
@@ -3,11 +3,13 @@ print(
 This is my first program
 """)
 x=raw_input('Please Enter:')
-try:
- y=float(x)
- if y%1>0:
-  print ('This is a float')
- else:
-  print ('This is an integer')
-except ValueError, e:
- print ('This is a string')
+while true:
+ try:
+  y=float(x)
+  if x=='end':break
+  if y%1>0:
+   print ('This is a float')
+  else:
+   print ('This is an integer')
+ except ValueError, e:
+  print ('This is a string')
--- yuhaozheng.py
+++ yuhaozheng.py
@@ -1,3 +1,7 @@
-print('hello world')
-print(2 ** 100)
-
+x = raw_input('please enter:')
+if int(x) != float(x):
+    print('a')
+else:
+    print('b')
+a = set(1, 2, 3, 4)
+a.pop(2)
