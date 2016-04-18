141ebf8cf5d5ac8d022640cdb44e8fb2bbd854f5
Yuhao <rogelio1026zyh@gmail.com>
2016-04-18 01:03:56 -0400

    need modify function

--- Warcraft_editor.py
+++ Warcraft_editor.py
@@ -1,20 +1,33 @@
 print("""This is a Warcraft editor!
 Lets create our HEROs.
 """)
+Hero_name={}
+A=['Blood_volume','Mana','Attack_force','Defense']
 while True:
     x=raw_input('His/Her/It name is:')
     if x=='end':break
-    Hero_name={}
     while not x in Hero_name:
-        ability=[]
-        A=['Blood volume','Mana','Attack force','Defense']
+        A_x=[]
         try:
-            for key in A:
-                B = raw_input("Hero's %s are:" % key)
+            for key1 in A:
+                B = raw_input("Hero's %s are:" % key1)
                 b=float(B)
-                ability.append(b)
-            print('%s is level 0, blood volume: %d, Mana: %d, Attack force: %d, Defense: %d' % (x, ability[0], ability[1],
-            ability[2], ability[3]))
+                A_x.append(b)
+                print(key1,'=>',b)
+            for key2 in A:
+                C = raw_input("Growing rate of %s for %s are:" % (key2,x))
+                c = float(C)
+                print('Growing rate of',key2,'=>',c)
+                A_x.append(c)
         except ValueError, e:
             print('Please enter again')
-        Hero_name.setdefault(x, ability)
+        Hero_name.setdefault(x, A_x)
+        l = Hero_name[x]
+    else:
+        try:
+            L=input('Your HERO is at level:')
+            for key3 in A:
+                l_plus = (l[A.index(key3) + 4] * (L - 1) + l[A.index(key3)])
+                print(key3, '=>', l_plus)
+        except ValueError, e:
+            print('Please enter again')
