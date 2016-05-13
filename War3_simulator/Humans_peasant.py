a76c4f7847f24193177066d0c1d957cf6e882290
Yuhao <rogelio1026zyh@gmail.com>
2016-05-14 00:30:46 +0800

    peasant 2.0

--- War3_simulator/Humans_peasant.py
+++ War3_simulator/Humans_peasant.py
@@ -1,4 +1,10 @@
-class Humans_Peasant:
+Key = ['max_hp', 'armor', 'attack', 'armor_type', 'attack_type', 'can_move', 'speed',
+       'can_attack', 'hp_regenration_rate', 'attack_range', 'food']
+class Unit:
+    def __init__(self, Key):
+        self.Key = Key
+
+class Humans_Peasant(Unit):
     max_hp = 220
     armor = 0
 
