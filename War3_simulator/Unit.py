f544aa9d7ec902b430424fcd1e653e608cc4fee6
Yuhao <rogelio1026zyh@gmail.com>
2016-05-25 21:04:18 +0800

    damage change for different armor

--- War3_simulator/Unit.py
+++ War3_simulator/Unit.py
@@ -2,30 +2,34 @@ import uuid
 import time
 
 armortype={'light':0,'medium':1,'heavy':2,'fortified':3,'hero':4,'unarmored':5}
+normal_attack = (1, 1.5, 1, .7, 1, 1)
+pierce_attack = (2, .75, 1, .35, .5, 1.5)
+siege_attack = (1, .5, 1, 1.5, .5, 1.5)
+magic_attack = (1.25, .75, 2, .35, .5, 1)
+chaos_attack = (1, 1, 1, 1, 1, 1)
+spell_attack = (1, 1, 1, 1, .7, 1)
+hero_attack = (1, 1, 1, .5, 1, 1)
+attack_type = {'normal': normal_attack, 'pierce': pierce_attack, 'siege': siege_attack, 'magic': magic_attack, 'chaos':
+    chaos_attack, 'spell': spell_attack, 'hero': hero_attack}
 class Unit:
-    def __init__(self, max_hp, attack, armor_type, hp_regeneration_rate = 1, hp_regenerate = True):
+    def __init__(self, max_hp=1, attack=0, armor_type='unarmored', armor=0, hp_regeneration_rate=1, hp_regenerate=True):
         self.name = uuid.uuid4()
         self.max_hp = max_hp
         self.attack = attack
         self.armor_type = armor_type
+        self.armor = armor
         self.current_hp = max_hp
         self.hp_regeneration_rate = hp_regeneration_rate
         self.hp_regenerate = hp_regenerate
 
     def underattacked(self,damage,underattack_type):
         self.damage = damage
-        normal_attack = (1, 1.5, 1, .7, 1, 1)
-        pierce_attack = (2, .75, 1, .35, .5, 1.5)
-        siege_attack = (1, .5, 1, 1.5, .5, 1.5)
-        magic_attack = (1.25, .75, 2, .35, .5, 1)
-        chaos_attack = (1, 1, 1, 1, 1, 1)
-        spell_attack = (1, 1, 1, 1, .7, 1)
-        hero_attack = (1, 1, 1, .5, 1, 1)
-        attack_type = {'normal': normal_attack, 'pierce': pierce_attack, 'siege': siege_attack, 'magic': magic_attack, 'chaos':
-                       chaos_attack, 'spell': spell_attack, 'hero': hero_attack}
         self.underattack_type = attack_type[underattack_type]
-        armortype = {'light': 0, 'medium': 1, 'heavy': 2, 'fortified': 3, 'hero': 4, 'unarmored': 5}
         self.damage_change = self.underattack_type[armortype[self.armor_type]]
+        if self.armor >= 0:
+            self.damage_change = self.damage_change * (1 - self.armor * 0.06 / (1 + self.armor * 0.06))
+        else:
+            self.damage_change = self.damage_change * (2 - 0.94^self.armor)
         self.current_hp = self.current_hp - self.damage * self.damage_change
         if self.current_hp < 0:
             self.current_hp = 0
