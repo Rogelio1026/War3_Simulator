ace0b114dd428d985f2970bc2e5b0abd67f81ef1
Yuhao <rogelio1026zyh@gmail.com>
2016-06-15 22:08:30 +0800

    troll_berserker test

--- Troll_Berserker.py
+++ Troll_Berserker.py
@@ -3,26 +3,26 @@ from Unit import Unit
 class Troll_berserker(Unit):
     def __init__(self):
         Unit.__init__(self,450,25,'pierce','medium',0,1,2.31,)
-        self.berserk = False
+        self.do_berserk = False
         self.berserk_cooldown = 30
         self.berserk_cooldown_remaining = 0
 
     def berserk(self):
         if self.berserk_cooldown_remaining == 0:
             self.launch_berserk()
-            self.berserk = True
+            self.do_berserk = True
 
     def launch_berserk(self):
         self.attack *= 2
 
     def underattacked(self, damage, underattack_type):
-        if self.berserk:
+        if self.do_berserk:
             damage = damage * 2
         Unit.underattacked(self, damage, underattack_type)
 
     def cancel_berserk(self):
         self.attack *= 0.5
-        self.berserk = False
+        self.do_berserk = False
 
     def berserk_reduce_cooldown(self, fps):
         self.berserk_cooldown_remaining -= 1 / fps
---
+++ Troll_Berserker_test.py
@@ -0,0 +1,20 @@
+import  unittest
+from Troll_Berserker import Troll_berserker
+from Humans_peasant import Peasant
+from clock import Game
+class TestStringMethods(unittest.TestCase):
+
+    def test_berserk(self):
+        game = Game()
+        troll_berserker = Troll_berserker()
+        peasant = Peasant()
+        game.add_recipients(troll_berserker)
+        self.assertEqual(troll_berserker.berserk_cooldown_remaining,0)
+        peasant.current_hp = 200
+        troll_berserker.force_attack(peasant)
+        self.assertEqual(peasant.current_hp,181.25)
+        troll_berserker.cooldown_remaining = 0
+        troll_berserker.berserk()
+        peasant.current_hp = 200
+        troll_berserker.force_attack(peasant)
+        self.assertEqual(peasant.current_hp, 162.5)
