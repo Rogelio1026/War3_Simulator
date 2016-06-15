73b566b754e60681e0154fdd44ddc20f0ae8c724
Yuhao <rogelio1026zyh@gmail.com>
2016-06-15 21:57:24 +0800

    Troll_berserker

---
+++ Troll_Berserker.py
@@ -0,0 +1,41 @@
+from Unit import Unit
+
+class Troll_berserker(Unit):
+    def __init__(self):
+        Unit.__init__(self,450,25,'pierce','medium',0,1,2.31,)
+        self.berserk = False
+        self.berserk_cooldown = 30
+        self.berserk_cooldown_remaining = 0
+
+    def berserk(self):
+        if self.berserk_cooldown_remaining == 0:
+            self.launch_berserk()
+            self.berserk = True
+
+    def launch_berserk(self):
+        self.attack *= 2
+
+    def underattacked(self, damage, underattack_type):
+        if self.berserk:
+            damage = damage * 2
+        Unit.underattacked(self, damage, underattack_type)
+
+    def cancel_berserk(self):
+        self.attack *= 0.5
+        self.berserk = False
+
+    def berserk_reduce_cooldown(self, fps):
+        self.berserk_cooldown_remaining -= 1 / fps
+        if self.berserk_cooldown_remaining - 12 < 0.001 \
+                and 0 < self.berserk_cooldown_remaining - 12:
+            self.cancel_berserk()
+        elif self.berserk_cooldown_remaining < 0.001:
+            self.berserk_cooldown_remaining = 0
+
+    def can_reduce_heal_cooldown(self):
+        if self.berserk_cooldown_remaining > 0:
+            return True
+
+    def spell_clock(self,fps):
+        if self.can_reduce_heal_cooldown():
+            self.berserk_reduce_cooldown(fps)
