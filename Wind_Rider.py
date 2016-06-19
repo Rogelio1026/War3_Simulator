9cba1a001e0eb342cef08d0e883af2b26684df81
Yuhao <rogelio1026zyh@gmail.com>
2016-06-19 23:14:15 +0800

    air vs air

---
+++ Wind_Rider.py
@@ -0,0 +1,13 @@
+from Unit import Unit
+from Attack import Attack
+
+
+class Wind_Rider(Unit):
+    def __init__(self):
+        Unit.__init__(self, 570, 40, 'pierce', 'light', 0, 1,2, 0,
+                      0, 'air', ['ground','building','air'])
+        self.air_attack = 40
+        self.air_attack_cooldown = 2
+        self.air_attack_type = 'pierce'
+
+
--- test/air_VS_ground.py
+++ test/air_VS_ground.py
@@ -1,6 +1,6 @@
 from Humans_peasant import Peasant
 from Flying_Machine import Flying_Machine
-from clock import Game
+from Wind_Rider import Wind_Rider
 from Unit import Unit
 from library.test_utility import compare_doubles
 import unittest
@@ -18,8 +18,17 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(flying_machine.current_hp, 200)
         flying_machine.flying_machine_bombs()
         flying_machine.launch_attack(peasant)
-        self.assertEqual(compare_doubles(peasant.current_hp, 214.375),True)
+        self.assertEqual(compare_doubles(peasant.current_hp, 216.25),True)
 
+    def test_air_vs_air(self):
+        flying_machine = Flying_Machine()
+        wind_rider = Wind_Rider()
+        self.assertEqual(flying_machine.current_hp, 200)
+        self.assertEqual(wind_rider.current_hp, 570)
+        flying_machine.launch_attack(wind_rider)
+        wind_rider.launch_attack(flying_machine)
+        self.assertEqual(compare_doubles(flying_machine.current_hp, 164.286),True)
+        self.assertEqual(wind_rider.current_hp, 541)
 
 if __name__ == '__main__':
     unittest.main()
