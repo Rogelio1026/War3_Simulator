79c370510f5f4fb08ff9ca016fa43eaf0e249ff3
Yuhao <rogelio1026zyh@gmail.com>
2016-06-20 23:37:55 +0800

    wind dider attack air

--- Wind_Rider.py
+++ Wind_Rider.py
@@ -10,4 +10,26 @@ class Wind_Rider(Unit):
         self.air_attack_cooldown = 2
         self.air_attack_type = 'pierce'
 
+def force_attack(self, enemy):
+    if enemy.position == 'air':
+        getAttack = Attack(self.air_attack, self.air_attack_type, self.name)
+        getAttack.sendAttack(enemy)
+    else:
+        Unit.force_attack(self, enemy)
 
+
+def launch_attack(self, enemy):
+    """
+
+    :param enemy: Unit
+    :return: function
+    """
+    if enemy.position in self.attackable_position:
+        if self.cooldown_remaining == 0:
+            self.force_attack(enemy)
+            if enemy.position == 'air':
+                self.cooldown_remaining = self.air_attack_cooldown
+            else:
+                self.cooldown_remaining = self.cooldown
+    else:
+        print('You cannot attack')
--- library/test_utility.py
+++ library/test_utility.py
@@ -1,3 +1,4 @@
 def compare_doubles(double1, double2):
     if abs(double1 - double2) <= 0.001:
         return True
+
